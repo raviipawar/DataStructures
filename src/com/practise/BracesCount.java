3f84c505794b64d90a5d5081afec6f9ed56a3223
ravindra <ravibalajipawar@gmail.com>
2020-08-20 23:29:50 +0530

    Changes

--- src/com/practise/BracesCount.java
+++ src/com/practise/BracesCount.java
@@ -1,38 +1,38 @@
-package com.practise;
-
-import java.util.Scanner;
-
-public class BracesCount {
-
-    public int matchedCount(){
-        Scanner scan = new Scanner(System.in);
-        Stack<Integer> stk = new Stack<Integer>();
-        System.out.println("Enter expression");
-        String exp = scan.next();
-        int len = exp.length();
-        System.out.println("\nMatches and Mismatches:\n");
-        int counter = 0;
-        for (int i = 0; i < len; i++)
-        {
-            char ch = exp.charAt(i);
-            if (ch == '(')
-                stk.push(i);
-            else if (ch == ')')
-            {
-                try
-                {
-                    int p = stk.pop() + 1;
-                    counter++;
-                }
-                catch(Exception e)
-                {
-                    return -1;
-                }
-            }
-        }
-
-        while (!stk.isEmpty() )
-            return -1;
-        return counter;
-    }
-}
+//package com.practise;
+//
+//import java.util.Scanner;
+//
+//public class BracesCount {
+//
+//    public int matchedCount(){
+//        Scanner scan = new Scanner(System.in);
+//        Stack stk = new Stack();
+//        System.out.println("Enter expression");
+//        String exp = scan.next();
+//        int len = exp.length();
+//        System.out.println("\nMatches and Mismatches:\n");
+//        int counter = 0;
+//        for (int i = 0; i < len; i++)
+//        {
+//            char ch = exp.charAt(i);
+//            if (ch == '(')
+//                stk.push(i);
+//            else if (ch == ')')
+//            {
+//                try
+//                {
+//                    int p = stk.pop() + 1;
+//                    counter++;
+//                }
+//                catch(Exception e)
+//                {
+//                    return -1;
+//                }
+//            }
+//        }
+//
+//        while (!stk.isEmpty() )
+//            return -1;
+//        return counter;
+//    }
+//}
